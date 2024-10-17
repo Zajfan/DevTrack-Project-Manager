2e6d9dbbfb9456ecaae44113de02b0de4af34198
Zajfan <114745912+Zajfan@users.noreply.github.com>
2024-10-17 05:01:35 +0200

    update

--- DevTrack.BLL/DataTransferObjects/ProjectDashboardData.cs
+++ DevTrack.BLL/DataTransferObjects/ProjectDashboardData.cs
@@ -1,14 +1,15 @@
 // ProjectDashboardData.cs
-using System.Reflection.Metadata;
+using DevTrack.DAL.Models;
+using DevTrack.DAL.Repositories;
 
 namespace DevTrack.BLL
 {
     public class ProjectDashboardData
     {
         public Project Project { get; set; }
-        public List<Task>? Tasks { get; set; }
-        public List<Milestone>? Milestones { get; set; }
-        public List<Document>? Documents { get; set; }
+        public List<Task> Tasks { get; set; } // Remove nullable operator (?)
+        public List<Milestone> Milestones { get; set; } // Remove nullable operator (?)
+        public List<DocumentRepository> Documents { get; set; } // Remove nullable operator (?)
         // ... add other properties for the dashboard data as needed
     }
 }
