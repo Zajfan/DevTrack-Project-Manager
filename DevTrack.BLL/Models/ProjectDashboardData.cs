54849e1f260e0d19ae92b7cd8ce7c0d1cbbb2839
Zajfan <114745912+Zajfan@users.noreply.github.com>
2024-10-18 06:10:41 +0200

    update

--- DevTrack.BLL/Models/ProjectDashboardData.cs
+++ DevTrack.BLL/Models/ProjectDashboardData.cs
@@ -1,16 +1,14 @@
 // ProjectDashboardData.cs
 using DevTrack.DAL.Models;
-using DevTrack.DAL.Repositories;
 using System.Collections.Generic;
 
 namespace DevTrack.BLL.Models
 {
     public class ProjectDashboardData
     {
-        public required Project Project { get; set; }
-        public required List<DevTrack.DAL.Models.Task> Tasks { get; set; }
+        public Project Project { get; set; }
+        public List<DevTrack.DAL.Models.Task> Tasks { get; set; }
         public List<Milestone> Milestones { get; set; }
-        public required List<DocumentRepository> Documents { get; set; }
-        // ... add other properties for the dashboard data as needed
+        public List<Document> Documents { get; set; } // Corrected type to List<Document>
     }
 }
