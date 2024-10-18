ef066c28170cdccef43f34746c02c4c591c70663
Zajfan <114745912+Zajfan@users.noreply.github.com>
2024-10-18 10:19:18 +0200

    update

--- DevTrack.DAL/Repositories/TaskRepository.cs
+++ DevTrack.DAL/Repositories/TaskRepository.cs
@@ -1,8 +1,8 @@
 using DevTrack.DAL.Models;
+using DevTrack.DAL.Repository;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace DevTrack.DAL.Repositories
 {
